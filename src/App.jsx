519f92496680ae56c4bc56f2b80cfed5af51c91e
Nicole Kening Li <likeningnicole@gmail.com>
2026-02-20 13:34:41 -0800

    Add firebase implementation and test swipe cards and accessing database items

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Mission from './components/Mission'
 import FAQ from './components/FAQ'
 import Contact from './components/Contact'
 import InterestForm from './components/InterestForm'
+import Match from './components/Match'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/faq" element={<FAQ />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/interest-form" element={<InterestForm />} />
+        <Route path="/match" element={<Match />} />
       </Routes>
     </BrowserRouter>
   );
